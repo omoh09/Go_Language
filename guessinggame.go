216dc92a4672fdd8264750350c576c74e88d86cb
omoh09 <gabrielifoga@yahoo.com>
2020-03-24 11:47:44 -0700

    Update guessinggame.go

--- guessinggame.go
+++ guessinggame.go
@@ -1,10 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	var input int
-	fmt.Println("Input decimal")
-	fmt.Scan(&input)
-	fmt.Printf("The binary is %b\n", input)
+	fmt.Println("Are you a genius...Lets play!")
+
+	//random number generator
+	source := rand.NewSource(time.Now().UnixNano())
+	randomizer := rand.New(source)
+	//generates numbers from 0 to 10 inclusive
+	secretNumber := randomizer.Intn(10)
+
+	var guess int
+	
+	//prompt user for number until game is won
+	for {
+		fmt.Println("What's the magic number?")
+		fmt.Scan(&guess)
+		if guess > secretNumber {
+			fmt.Println("That's bigger")
+		} else if guess < secretNumber {
+			fmt.Println("That's smaller")
+		} else {
+			fmt.Println("Awesome, you got it!")
+			break
+		}
+	}
 }
